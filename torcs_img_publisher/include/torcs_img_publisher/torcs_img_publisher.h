b0e4a697a27f59d9d3a0fa8b684e619502bd930b
Florian Mirus <florian.mirus@gmx.net>
2017-03-29 10:08:38 +0200

    first working version of an image publisher to stream the pov image of the simulator into the ROS system

---
+++ torcs_img_publisher/include/torcs_img_publisher/torcs_img_publisher.h
@@ -0,0 +1,67 @@
+#ifndef TORCS_IMAGE_PUBLISHER_NODE_H
+#define TORCS_IMAGE_PUBLISHER_NODE_H
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <stdlib.h>  
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+// ROS includes
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+
+#define image_width 640
+#define image_height 480
+
+struct shared_use_st  
+{  
+  int written;
+  unsigned char data[image_width*image_height*3];
+  int pause;
+  int zmq_flag; 
+  int save_flag; 
+};
+
+class TORCSImgPublisherNode{
+private:
+  struct config_struct{
+    int resize_width, resize_height;
+    double loop_rate;
+    int paused;
+  };
+  void *shm = NULL;
+  struct shared_use_st *shared_;
+  int shmid;
+
+  // Setup opencv
+  IplImage* screenRGB_;
+  IplImage* resizeRGB_;
+
+  config_struct config_;
+
+public:
+  
+  TORCSImgPublisherNode();
+
+  ~TORCSImgPublisherNode();
+
+  ros::NodeHandle nh_, pnh_;
+  image_transport::ImageTransport it_;
+
+  image_transport::Publisher image_publisher_;
+
+  void update();
+
+  void getParams();
+
+};
+
+#endif
---
+++ torcs_img_publisher/src/torcs_img_publisher.cpp
@@ -0,0 +1,115 @@
+#include <torcs_img_publisher/torcs_img_publisher.h>
+
+// constructor
+TORCSImgPublisherNode::TORCSImgPublisherNode()
+ : it_(nh_)
+{
+  ROS_DEBUG("init torcs image publisher node");
+  // create node handles
+  nh_ = ros::NodeHandle();
+  pnh_ = ros::NodeHandle("~");
+
+  ROS_DEBUG("Start Memory sharing");
+  shmid = shmget((key_t)1234, sizeof(struct shared_use_st), 0666|IPC_CREAT);
+  if(shmid == -1)  
+  {  
+    fprintf(stderr, "shmget failed\n");  
+    exit(EXIT_FAILURE);  
+  }
+  shm = shmat(shmid, 0, 0);  
+  if(shm == (void*)-1)  
+  {  
+    fprintf(stderr, "shmat failed\n");  
+    exit(EXIT_FAILURE);  
+  }  
+  ROS_DEBUG_STREAM("Memroy sharing started at " << shm);
+  // printf("\n********** Memory sharing started, attached at blablabal %X **********\n", shm);
+
+  getParams();
+
+  shared_ = (struct shared_use_st*)shm; 
+  shared_->written = 0;
+  shared_->pause = config_.paused;
+  shared_->zmq_flag = 0;  
+  shared_->save_flag = 0;
+
+  // Setup opencv
+  screenRGB_ = cvCreateImage(cvSize(image_width,image_height),IPL_DEPTH_8U,3);
+  resizeRGB_ = cvCreateImage(cvSize(config_.resize_width,config_.resize_height),IPL_DEPTH_8U,3);
+
+  // publisher
+  image_publisher_ = it_.advertise("/torcs/image", 1);
+
+  // cv::namedWindow("TORCS Image");
+}
+
+TORCSImgPublisherNode::~TORCSImgPublisherNode(){
+  // cv::destroyWindow("TORCS Image");
+}
+
+void TORCSImgPublisherNode::getParams()
+{
+  if(!pnh_.hasParam("loop_rate"))
+    {
+      ROS_WARN("No parameter loop_rate on parameter server. Using default value [10.0 Hz].");
+    }
+  pnh_.param("loop_rate", config_.loop_rate, (double)10.0);
+
+  if(!pnh_.hasParam("resize_width"))
+    {
+      ROS_WARN("No parameter resize_width on parameter server. Using default value [640 px].");
+    }
+  pnh_.param("resize_width", config_.resize_width, (int)640);
+
+  if(!pnh_.hasParam("resize_height"))
+    {
+      ROS_WARN("No parameter resize_height on parameter server. Using default value [480 px].");
+    }
+  pnh_.param("resize_height", config_.resize_height, (int)480);
+
+  if(!pnh_.hasParam("paused"))
+    {
+      ROS_WARN("No parameter paused on parameter server. Using default value [1].");
+    }
+  pnh_.param("paused", config_.paused, (int)1);
+}
+
+void TORCSImgPublisherNode::update()
+{
+  if (shared_->written == 1) {
+
+    for (int h = 0; h < image_height; h++) {
+      for (int w = 0; w < image_width; w++) {
+       screenRGB_->imageData[(h*image_width+w)*3+2]=shared_->data[((image_height-h-1)*image_width+w)*3+0];
+       screenRGB_->imageData[(h*image_width+w)*3+1]=shared_->data[((image_height-h-1)*image_width+w)*3+1];
+       screenRGB_->imageData[(h*image_width+w)*3+0]=shared_->data[((image_height-h-1)*image_width+w)*3+2];
+      }
+    }
+    
+    cvResize(screenRGB_, resizeRGB_);
+    
+    Mat img = cvarrToMat(resizeRGB_, true);
+    // Update GUI Window
+    // cv::imshow("TORCS Image", img);
+    // cv::waitKey(3);
+  
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
+    
+    image_publisher_.publish(msg);
+
+    shared_->written=0;
+  }
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "torcs_img_publisher_node");
+  TORCSImgPublisherNode my_publsiher;
+  ros::Rate loop_rate(100); // Hz
+  while(my_publsiher.nh_.ok()){
+    my_publsiher.update();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
