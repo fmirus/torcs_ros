55c3da99e393043c8365228809b1fe0d13993066
B-Zorn <benni@the-zorns.de>
2018-07-23 10:32:29 +0200

    Added trajectory generation and transformation v1

---
+++ torcs_ros_trajectory/src/Trajectory_generation.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 13:09:55 2018
+
+@author: bzorn
+"""
+
+import matplotlib.pyplot as plt
+import scipy.interpolate
+import numpy as np
+
+
+def curve(a1, a2, x):
+    y = s0+a1*x*x+a2*x
+    if abs(y) <= 1:
+        return y
+    else:
+        return np.sign(y)*1 
+    
+def CurveTrajectories():
+    s0 = 0.8
+    
+    t1 = 3
+    t2 = 5
+    # s_out = so+ a1*t*t 
+    xnew = np.linspace(0, 5, num=41)
+    
+    
+    #Network determines goal steering, and time frame interpolation done manually (could be implemented with or without action choice)
+    for s1 in np.linspace(-1, 1, 10):
+        rand = 5
+        while (rand > (t2-t1) or rand < -t1):
+            rand = np.random.normal(loc=0, scale=2)
+        x = np.array([0, t1+rand, t2])
+        y = np.array([s0, s1, s1])
+        f2 = scipy.interpolate.interp1d(x, y, kind='quadratic')
+        ynew = [f2(x) if abs(f2(x))<= 1 else np.sign(f2(x))*1 for x in xnew]
+        plt.plot(xnew, ynew)
+    plt.title('Interp1d: Quadratic')
+    plt.show()
+        #smooth cubic interpolation
+    for s1 in np.linspace(-1, 1, 50):
+        x = [0, t2]
+        y = [s0, s1]
+        f = scipy.interpolate.CubicSpline(x,y, bc_type='clamped')
+        ynew = [f(x) if abs(f(x))<= 1 else np.sign(f(x))*1 for x in xnew]
+        plt.plot(xnew, f(xnew))
+    plt.title('CubicSpline - Clamped')
+    plt.show()
+    
+    for s1 in np.linspace(-1, 1, 50):
+        rand = 5
+        while (rand > (t2-t1) or rand < -t1):
+            rand = np.random.normal(loc=0, scale=0.8)
+        x = [0, t1+rand, t2]
+        y = [s0, s1, s1]
+        f = scipy.interpolate.CubicSpline(x,y, bc_type='clamped')
+        ynew = [f(x) if abs(f(x))<= 1 else np.sign(f(x))*1 for x in xnew]
+        plt.plot(xnew, ynew)
+    plt.title('CubicSpline - Clamped - Noisy')
+    plt.show()
+    #Network decides between set of parameters (Calculation can then be performed by a network) --> entirely possible in neuromorph
+    for n in np.linspace(-1, 1, 50):
+        a1 = np.random.uniform(-0.05, 0.05)
+        a2 = np.random.uniform(-0.05, 0.05)
+        ynew = [curve(a1, a2, x) for x in xnew]
+        plt.plot(xnew, ynew)
+    plt.title('Parametrized')
+    plt.show()
+
+#Width should be set according to expected track width
+def EgoTrajectories(f_lateralDist, f_longitudinalDist, n_amount, b_verbose):
+
+    xnew = np.linspace(0, f_longitudinalDist, num=41)
+
+    Traj_Cubic = []
+    Traj_Quad = []
+    
+    for s1 in np.linspace(-float(f_lateralDist)/2, float(f_lateralDist)/2, 10):
+        x = [0, f_longitudinalDist]
+        y = [0, s1]
+        f = scipy.interpolate.CubicSpline(x,y, bc_type='clamped')
+        ynew1 = [f(x) if abs(f(x))<= 1 else np.sign(f(x))*1 for x in xnew]
+#        plt.plot(f(xnew), xnew)
+        Traj_Cubic.append(f(xnew))
+    if b_verbose is True:
+        plt.title('EgoTrajectories - Cubic Clamped')
+        plt.xlabel('[m]')
+        plt.ylabel('[m]')
+        plt.show()
+        
+    for s1 in np.linspace(-float(f_lateralDist)/2, float(f_lateralDist)/2, 10):
+        x = [0, float(f_longitudinalDist)/2+float(f_longitudinalDist)/10, f_longitudinalDist]
+        y = [0, s1/2, s1]
+        f = scipy.interpolate.interp1d(x, y, kind='quadratic')
+        ynew2 = [f(x) if abs(f(x))<= 1 else np.sign(f(x))*1 for x in xnew]
+#        plt.plot(f(xnew), xnew)
+        Traj_Quad.append(f(xnew))
+    
+    if b_verbose is True:
+        plt.title('EgoTrajectories - Quadratic')
+        plt.xlabel('[m]')
+        plt.ylabel('[m]')
+        plt.show()
+    
+        [plt.plot(trajectory, xnew, c='r', alpha=0.8) for trajectory in Traj_Cubic]
+        [plt.plot(trajectory, xnew, c='b', alpha=0.5) for trajectory in Traj_Quad]
+        plt.title('EgoTrajectories - All')
+        plt.xlabel('[m]')
+        plt.ylabel('[m]')
+        plt.show()
+        
+    return [xnew, np.concatenate((Traj_Cubic, Traj_Quad), axis=0)]
+
+
+def CompareSecondOrderPolynomials():
+    a, b, c = 1,1, 1
+    x = np.linspace(0, 1, 10)
+    [plt.plot((a+n)*x*x+b*x+c, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('a*x*x')
+    plt.show()
+    [plt.plot((a)*x*x+(b+n)*x+c, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('b*n')
+    plt.show()
+    [plt.plot(a*x*x+b*x+c+n, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('c')
+    plt.show()
+
+
+#    b = 5
+#    [plt.plot((a+n)*x*x+b*x+c, x) for n in np.linspace(-5, 5, 20)]
+#    plt.title('a*x*x')
+#    plt.show()
+
+# jegliches Polynom kann so rotiert werden, dass es stetig an ein anderes angeknüpft werden kann
+def CompareThirdOrderPolynomials():
+    a, b, c, d = 1, 1, 1, 1
+    x = np.linspace(0, 1, 10)
+    [plt.plot((d+n)*x*x*x+a*x*x+b*x+c, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('d*x*x*x')
+    plt.show()
+    [plt.plot(d*x*x*x+(a+n)*x*x+b*x+c, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('a*x*x')
+    plt.show()
+    [plt.plot(d*x*x*x+(a)*x*x+(b+n)*x+c, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('b*n')
+    plt.show()
+    [plt.plot(d*x*x*x+(a+n)*x*x+b*x+c+n, x) for n in np.linspace(-5, 5, 20)]
+    plt.title('c')
+    plt.show()
+    
+#EgoTrajectories()
+#CompareSecondOrderPolynomials()
+#CompareThirdOrderPolynomials()
---
+++ torcs_ros_trajectory/src/torcs_ros_trajectory.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 10:18:44 2018
+
+@author: bzorn
+"""
+
+import numpy as np
+import rospy
+import tf
+
+## ros messages
+from geometry_msgs.msg import TwistStamped, Vector3Stamped, PoseStamped, Point, PointStamped, TransformStamped, Quaternion
+from visualization_msgs.msg import Marker
+from tf2_msgs.msg import TFMessage
+from nav_msgs.msg import Path
+import Trajectory_generation
+import copy
+
+import time
+
+
+#Vector class to manually store translations
+class vec3():
+    def __init__(self, i_x=0, i_y=0, i_z=0):
+        self.x,self.y,self.z = i_x,i_y,i_z
+    def Set(self, msg_frame):
+        self.x = msg_frame.transform.translation.x  
+        self.y = msg_frame.transform.translation.y
+        self.z = msg_frame.transform.translation.z
+    def __getitem__(self, i):
+        vec = [self.x, self.y, self.z]
+        return vec[i]
+#Vector class to manually store quaternion orientations
+class vec4():
+    def __init__(self, i_x=0, i_y=0, i_z=0, i_w=1):
+        self.x,self.y,self.z,self.w = i_x,i_y,i_z,i_w
+    def Set(self, msg_frame):
+        self.x = msg_frame.transform.rotation.x  
+        self.y = msg_frame.transform.rotation.y
+        self.z = msg_frame.transform.rotation.z
+        self.w = msg_frame.transform.rotation.w
+    def __getitem__(self, i):
+        vec = [self.x, self.y, self.z, self.w]
+        return vec[i]
+
+#ROS node functionality
+class TrajectoryPublish():
+    def __init__(self, frame_topic = "/tf"):#frame_topic="/torcs_ros/torcs_global_pose"):
+        #### parameters for trajectory generation ####
+        self.f_lateralDist = 10 #lateral spread in width between terminal points in metre
+        self.f_longitudinalDist = 10  #planning horizon in metre
+        self.n_amount = 10 #amount of trajectories per class
+        
+        #### subscribers ####
+        self.sub_frame = rospy.Subscriber(frame_topic, TFMessage, self.sub_frame_callback, queue_size=1) #a subscriber that manually subscribes to the published frames
+        self.frame = tf.TransformListener() #tf "Subscriber" that can grab different transformations on call
+        
+        #### publishers ####
+        self.pub_trajectory = rospy.Publisher("/torcs_ros/trajectory", Marker, queue_size=1) #publisher that publishes trajectories as Markers //depreceated
+         #list of publishers of trajectories in Path() format, one for each trajectory
+        self.pub_allPaths = []
+        [self.pub_allPaths.append(rospy.Publisher("/torcs_ros/trajectory"+str(x+1), Path, queue_size=1)) for x in range(0, self.n_amount*2)]
+        self.pub_pathSelected = rospy.Publisher("/torcs_ros/trajectorySelected", Path, queue_size=1) #publisher for currently selected trajectory to be followed
+        self.fram_pub = tf.TransformBroadcaster()
+        #### message definitions #####
+        self.trajectoryBaselink_msgs = [] #trajectories in baselink coordinates (static) //depreceated
+        self.trajectoryWorld_msgs = [] #trajectories in world coordinates (static) //depreceated
+        self.path_msgs = [] #trajectories as paths in baselink coordinates 
+        self.pathWorld_msgs = []
+        self.selectedTrajectory_msg = Path() #currently selected trajectory to be followed
+        
+        #### trajectory parameters #### 
+        self.n_selectedTrajectory = 0 #trajectory to be followed currently
+        #initialization of static trajectory values
+        self.y_vals, self.x_vals = [], [] #declaration needed as set_trajectories throws error otherwise
+        [self.x_vals, self.y_vals] = Trajectory_generation.EgoTrajectories(self.f_lateralDist, 
+            self.f_longitudinalDist, self.n_amount, False)
+        self.set_trajectories() #initialize trajectories
+
+        #### remaining parameters ####
+        self.time = rospy.Time.now() #to be used as reference time when performing transformations; not working yet
+        self.tf_trans = [] #a transformations translatory values received from tr TransformListener
+        self.tf_rot = [] #a transformations rotatory values received from tr TransformListener
+        self.ros_trans = vec3() #a transformations translatory values received from ros subscription
+        self.ros_rot = vec4() #a transformations rotatory values received from ros subscription
+        self.newest_ros_transform = TransformStamped();
+
+        
+        
+        
+    def set_trajectories(self):
+        ########## Path version of trajectories ##############
+        self.path_msgs = [] #cleanup list for new calculation        
+        ctime = rospy.Time.now() #assign all paths the same time
+        #create one message for each trajectory
+        for f in range(0, len(self.y_vals)):
+            self.path_msgs.append(Path()) #push the Path() message type to list
+            self.path_msgs[-1].header.stamp = ctime #assign same timestamp to all trajectories
+            self.path_msgs[-1].header.frame_id = "base_link" #definition is in base_link to account for vehicles position and orientation
+            #create poses for current path message 
+            for ff in range (0, len(self.x_vals)):
+                pose = PoseStamped() #PoseStamped needed for Path() message
+                pose.header.stamp = ctime #Assign all poses same timestamp as path
+                pose.header.frame_id = "base_link" #define poses in same frame as path
+                pose.pose.position.x = self.x_vals[ff] #get x value from list
+                pose.pose.position.y = self.y_vals[f][ff] #get y value from list
+                pose.pose.position.z = 0 #set z value to 0 as we will ignore it
+                #roll pitch and yaw needed for pose orientation, not filled as of yet
+                roll = 0 #as trajectories are defined in 2D we can neglect this
+                pitch = 0 #as trajectories are defined in 2D we can neglect this
+                yaw = 0 #angle about z, orientation of current pose should be looking towards next post location
+#                pose.pose.rotation = tf.transformations.quaternion_from_euler(roll, pitch, yaw) #convert rpy to quaternion
+                self.path_msgs[-1].poses.append(pose)#append pose to current path message
+        
+        
+        
+        
+    #mark a trajectory as active and visualize it in rviz with a different color by publishing it to another topic
+    def mark_trajectory_i_as_active(self, trajectory):
+        self.n_selectedTrajectory = trajectory #mark selected trajectory
+        self.selectedTrajectory_msg = copy.copy(self.pathWorld_msgs[self.n_selectedTrajectory]) #Move message to selectedTrajectory publisher message
+        self.pathWorld_msgs[self.n_selectedTrajectory].poses = [] #ensure old message does not interfere in visualization
+
+    #manually transform trajectories from baselink to world frame
+    def transform_trajectories(self):
+        self.pathWorld_msgs = [] #cleanup previous message
+        self.ros_rot_mat = tf.transformations.quaternion_matrix(self.ros_rot) #calculate rotation matrix from quaternion
+        for pathCounter in range(0, len(self.path_msgs)): #iterate over all trajectories
+            self.pathWorld_msgs.append(copy.deepcopy(self.path_msgs[pathCounter])) #create a new message with a trajectory as content
+            self.pathWorld_msgs[-1].header.frame_id = 'world' #set that trajectories frame to world instead
+            #transform trajectory poses to current base_link position
+            for pose in self.pathWorld_msgs[-1].poses:
+                pose.header.frame_id = 'world' #set PoseStamped Header
+                #rotate pose by quaternion with a rotation matrix
+                #results are same when comparing to rotating v1 by quaternion q1 with q1*v1*(q1^*)
+                position_homogenous = np.asarray([pose.pose.position.x, pose.pose.position.y, pose.pose.position.z, 1]) #make a homogenous position vector, needed as rot. matrix is 4x4
+                position_rotated = np.matmul(self.ros_rot_mat, position_homogenous) #implementation with matrix is neglible faster than quaternion implementation
+                #translate rotated postion to baselink origin    
+                pose.pose.position.x = position_rotated[0] + self.ros_trans.x 
+                pose.pose.position.y = position_rotated[1] + self.ros_trans.y
+                pose.pose.position.z = position_rotated[2] + self.ros_trans.z
+                
+
+    #callback function that gets the baselink transformation and publishes the transformed trajectories in the world frame          
+    def sub_frame_callback(self, msg_frame):
+        frame_idx = 0 #index to identify the proper transformation if there is more than the world and base_link frames avalaible
+        b_frameHasBeenFound = False #flag to show if transformation has been found
+        #identify transformation
+        for frame in msg_frame.transforms:
+            if frame.header.frame_id == "world" and frame.child_frame_id == "base_link":
+                b_frameHasBeenFound = True #set flag that a valid transformation has been published
+                break;
+            ++frame_idx
+#        
+        if b_frameHasBeenFound == True: #ensure there is data to use
+            self.newest_ros_transform = msg_frame.transforms[frame_idx] #get transform from message
+            self.time = msg_frame.transforms[frame_idx].header.stamp #get time from message
+            self.ros_trans.Set(msg_frame.transforms[frame_idx]) #Get translation from published ros message
+            self.ros_rot.Set(msg_frame.transforms[frame_idx]) #Get rotation from published ros message
+          
+        self.transform_trajectories() #transform trajectories to world coordinates
+        self.mark_trajectory_i_as_active(8) #set a chosen trajectory as active
+
+        #publish all trajectories apart from selected one (message is empty)
+        for path_msg, path_pub in zip (self.pathWorld_msgs, self.pub_allPaths):
+            if path_msg is not self.path_msgs[self.n_selectedTrajectory]:
+                path_pub.publish(path_msg)
+
+        
+        self.pub_pathSelected.publish(self.selectedTrajectory_msg) #publish selected trajectory
+        #print some kind of indicator that a new step has been reached
+        randint = np.random.randint(7)
+        print(int(randint)*".")
+
+
+
+        
+        
+if __name__ == '__main__':
+    rospy.init_node('Trajectory_Publisher')
+    TrajectoryPublisher = TrajectoryPublish()
+    rospy.spin() 
+
