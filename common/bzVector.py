3f2123d468a54a910782bf31dbf382e0895264e7
B-Zorn <benni@the-zorns.de>
2018-07-23 15:57:43 +0200

    Added new folder with shared functions

---
+++ common/bzVector.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 23 15:51:19 2018
+
+@author: bzorn
+"""
+
+#Vector class to manually store translations
+class vec3():
+    def __init__(self, i_x=0, i_y=0, i_z=0):
+        self.x,self.y,self.z = i_x,i_y,i_z
+    def Set(self, msg_frame):
+        self.x = msg_frame.transform.translation.x  
+        self.y = msg_frame.transform.translation.y
+        self.z = msg_frame.transform.translation.z
+    def __getitem__(self, i):
+        vec = [self.x, self.y, self.z]
+        return vec[i]
+#Vector class to manually store quaternion orientations
+class vec4():
+    def __init__(self, i_x=0, i_y=0, i_z=0, i_w=1):
+        self.x,self.y,self.z,self.w = i_x,i_y,i_z,i_w
+    def Set(self, msg_frame):
+        self.x = msg_frame.transform.rotation.x  
+        self.y = msg_frame.transform.rotation.y
+        self.z = msg_frame.transform.rotation.z
+        self.w = msg_frame.transform.rotation.w
+    def __getitem__(self, i):
+        vec = [self.x, self.y, self.z, self.w]
+        return vec[i]
--- torcs_ros_trajectory_gen/src/torcs_ros_trajectory.py
+++ torcs_ros_trajectory_gen/src/torcs_ros_trajectory.py
@@ -18,33 +18,12 @@ from nav_msgs.msg import Path
 import Trajectory_generation
 import copy
 
-import time
+import os
+import sys
+cwd = os.getcwd()
+sys.path.append(cwd[:-29] + "common")
 
-
-#Vector class to manually store translations
-class vec3():
-    def __init__(self, i_x=0, i_y=0, i_z=0):
-        self.x,self.y,self.z = i_x,i_y,i_z
-    def Set(self, msg_frame):
-        self.x = msg_frame.transform.translation.x  
-        self.y = msg_frame.transform.translation.y
-        self.z = msg_frame.transform.translation.z
-    def __getitem__(self, i):
-        vec = [self.x, self.y, self.z]
-        return vec[i]
-#Vector class to manually store quaternion orientations
-class vec4():
-    def __init__(self, i_x=0, i_y=0, i_z=0, i_w=1):
-        self.x,self.y,self.z,self.w = i_x,i_y,i_z,i_w
-    def Set(self, msg_frame):
-        self.x = msg_frame.transform.rotation.x  
-        self.y = msg_frame.transform.rotation.y
-        self.z = msg_frame.transform.rotation.z
-        self.w = msg_frame.transform.rotation.w
-    def __getitem__(self, i):
-        vec = [self.x, self.y, self.z, self.w]
-        return vec[i]
-    
+from bzVector import vec3, vec4 #2d and 3d vector definitions    
 
 
 #ROS node functionality
