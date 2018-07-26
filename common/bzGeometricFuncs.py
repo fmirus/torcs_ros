6266396003fda9209280e11fa79add8b93417181
B-Zorn <benni@the-zorns.de>
2018-07-26 10:03:09 +0200

    trajectory_ctrl now launched on bringup if flag driver:=false

--- common/bzGeometricFuncs.py
+++ common/bzGeometricFuncs.py
@@ -16,56 +16,62 @@ import shapely.geometry as geo
 #def BaseLinkToTrajectory(baselink_x, baselink_y, trajectory_xvals, trajectory_yvals):
 def BaseLinkToTrajectory(baselink_x, baselink_y, trajectory_poses):
     
-    [trajectory_xvals, trajectory_yvals] = PathToValues(trajectory_poses)
+    [trajectory_xvals, trajectory_yvals] = PathToValues(trajectory_poses) #get list of position values from path message
     
-    geoBaselink = geo.Point(baselink_x, baselink_y)
-    trajectory = np.array([trajectory_xvals, trajectory_yvals])
-    trajectory = np.transpose(trajectory)
-    geoTrajectory = geo.LineString(trajectory)
+    geoBaselink = geo.Point(baselink_x, baselink_y) #create a shapely point entity for the baselink position
+    trajectory = np.array([trajectory_xvals, trajectory_yvals]) #recombine trajectory points to numpy array
+    trajectory = np.transpose(trajectory) #transpose needed for shapely
+    geoTrajectory = geo.LineString(trajectory) #create a shapely polyline entity for the trajectory
     f_dist = geoBaselink.distance(geoTrajectory) #distance between trajectory and baselink origin
     f_projLength = geoTrajectory.project(geoBaselink, normalized=True) #projection of Baselink to closest point on trajectory with distance f_dist
+    f_distToEnd = geo.Point(geoTrajectory.coords[-1]).distance(geoBaselink) #result how far from end of trajectory current baselink is located
     
-    f_distToEnd = geo.Point(geoTrajectory.coords[-1]).distance(geoBaselink)
-    
-    b_DirectionFound = False
-    nDirection = 0
-    idx = int(round(len(geoTrajectory.coords)*f_projLength))
-    while(True):
-#        if idx == 0: #is starting point
-#            idx += 1
-        if idx == len(geoTrajectory.coords)-1:
-            idx -= 1
-            print("at estimated end")
+    #we are going to search for the nearest defined polyline point in order to determine the vehicle's desired heading
+    #this is done by finding its bounding points (comparing two points projected length to the projected length of the point;
+    #if one is higher and one lower we have identified the bounding points)
+    #in order to save some time we will estimate a starting point and move along the polyline until we can identify the nearest point
+    b_DirectionFound = False #indicator flag to perform a DoOnce operation
+    nDirection = 0 #indicates whether search direction is towards start or end of polyline
+    idx = int(round(len(geoTrajectory.coords)*f_projLength)) #currently inspected index, initialization with normalized proj_length to estimate a starting point that might be close
+    idx = np.clip(idx, 0, len(geoTrajectory.coords)-1) #limit to valid range
+    while(True): #repeat until nearest point is identified
+        if idx >= len(geoTrajectory.coords)-1: #consider boundary case where last point was assumed as starting point
+            idx -= 1 #consider penultimate point instead //was just -1
         else:
-            if(b_DirectionFound == False):
-                f_projLengthPointBefore = geoTrajectory.project(geo.Point(geoTrajectory.coords[idx]), normalized=True)
+            if(b_DirectionFound == False): #see if this is the first loop
                 try:
-                    f_projLengthPointAfter = geoTrajectory.project(geo.Point(geoTrajectory.coords[idx+1]), normalized=True)
+                    f_projLengthPointBefore = geoTrajectory.project(geo.Point(geoTrajectory.coords[idx]), normalized=True) #get the lenght along the line of idx
                 except:
-                    print("ERROR idx: " +str(idx))
-            elif(nDirection == 1):
+                    print("ERROR idx in before calc: " +str(idx))     
+                try: 
+                    f_projLengthPointAfter = geoTrajectory.project(geo.Point(geoTrajectory.coords[idx+1]), normalized=True) #get the length along the line of next point for comparison
+                except:
+                    print("ERROR idx in after calc: " +str(idx))
+            elif(nDirection == 1): #direction is towards end, calculate next needed length
                 f_projLengthPointAfter = geoTrajectory.project(geo.Point(geoTrajectory.coords[idx+1]), normalized=True)
-            else:
+            else: #direction is towards start, calculate next needed length
                 f_projLengthPointBefore = geoTrajectory.project(geo.Point(geoTrajectory.coords[idx]), normalized=True)
                 
-            if (f_projLengthPointBefore <= f_projLength and f_projLengthPointAfter >= f_projLength):
-                idx += 1
+            if (f_projLengthPointBefore <= f_projLength and f_projLengthPointAfter >= f_projLength): #check whether the projected point is bounded by the two current points
+                idx += 1 #point has been identified, return after point idx (as this will be the desired heading)
                 break;
             else:
-                if (nDirection == 0):
+                if (nDirection == 0): #first loop
+                    #see whether baselink projection point is closer to after or before point and set direction to the lower one
                     if(f_projLengthPointAfter-f_projLength < f_projLengthPointBefore-f_projLength):
                         nDirection = 1
                     else:
                         nDirection = -1
-                    b_DirectionFound = True
+                    b_DirectionFound = True #set flag
+            #move along line in dependent projection
             if(nDirection == 1):
                 idx += 1
-                f_projLengthPointBefore = f_projLengthPointAfter
+                f_projLengthPointBefore = f_projLengthPointAfter  #we can reuse the previously calculated value of the after-point, as it is now the before-point
                 if(idx == len(geoTrajectory.coords)-1):
                     break; #last point has been reached
             elif(nDirection == -1):
                 idx -= 1
-                f_projLengthPointAfter = f_projLengthPointBefore
+                f_projLengthPointAfter = f_projLengthPointBefore# reuse before-point s after-point
 
     #so far only the distance has been calculated. the side of the trajectory the baselink is located on has to be accounted for as well
     #in 2D this can be determined by the sign of the cross product of two vectors
@@ -77,9 +83,16 @@ def BaseLinkToTrajectory(baselink_x, baselink_y, trajectory_poses):
     AP = np.array((baselink_x-trajectory_xvals[idx-1], baselink_y-trajectory_yvals[idx-1]))
     f_dist = f_dist*np.sign(np.cross(AB, AP))*-1
     
-    return [f_dist, idx, f_distToEnd]
+    #check whether the end of the trajectory could not be reached
+    #this can be the case if there is walls in the track that the laser scanners do not pick up
+    if (f_projLength == 1.0 and f_distToEnd > 2 ): 
+        b_OutOfRange = True
+    else:
+        b_OutOfRange = False
+    
+    return [f_dist, idx, f_distToEnd, b_OutOfRange]
 
-
+#returns list of x and y values of a paths pose.positions
 def PathToValues(trajectory_poses):
     xvals = []
     yvals = []
--- torcs_ros_trajectory_ctrl/src/torcs_ros_trajectory_ctrl.py
+++ torcs_ros_trajectory_ctrl/src/torcs_ros_trajectory_ctrl.py
@@ -22,8 +22,13 @@ from std_msgs.msg import Bool
 #add common folder to path; have to replace this later with rospkg when launched with roslaunch 
 import os
 import sys
-cwd = os.getcwd()
-sys.path.append(cwd[:-29] + "common")
+#cwd = os.getcwd()
+#sys.path.append(cwd[:-29] + "common")
+
+import rospkg
+cwd = rospkg.RosPack().get_path('torcs_ros_trajectory_ctrl')
+sys.path.append(cwd[:-25] + "common")
+print(cwd[:-25] + "common")
 
 from bzVector import vec3, vec4#2d and 3d vector definitions
 from bzGeometricFuncs import BaseLinkToTrajectory
@@ -32,8 +37,8 @@ import bzConsoleIndicators
 
 class FollowTrajectory():
     def __init__(self, trajectory_topic="/torcs_ros/trajectorySelected", ctrl_topic = "/torcs_ros/ctrl_cmd", 
-                 frame_topic="/tf", sensors_topic="/torcs_ros/sensors_state"):
-        self.sensors_topic = sensors_topic
+                 frame_topic="/tf", sensors_topic="/torcs_ros/sensors_state", speed_topic = "/torcs_ros/speed"):
+        
 
         #### subscription parameters ####
         self.ros_trans = vec3() #global position of baselink frame
@@ -44,6 +49,7 @@ class FollowTrajectory():
         self.sen_trackpos = 0 #delta x to mid line normalized
         self.sen_rpm = 0 #engine rmp
         self.sen_gear = 0 #current gear
+        self.sen_speedX = 0 #cars longitudinal speed
         
         #### publication parameters ####
         self.ctrl_accel = 0 #desired accel signal [calculated for low constant speed]
@@ -53,25 +59,26 @@ class FollowTrajectory():
         self.needForAction_msg = Bool()
         
         #### calculation parameters and variables #### 
-        self.param_kappa = 0.75 #value used to adjust desired heading to 
+        self.param_kappa = 0.75/2 #value used to adjust desired heading to, experimentlly determined. 0.75 lead to small oscillation
         self.param_steerLock = np.deg2rad(21) #max. steerLock in torcs/data/cars/tbt1 is 21 deg
         self.trajectory_rot = vec4() #desired orientation from trajectory 
-       
+        self.sensors_topic = sensors_topic #message type used to identify whether the client node has been restarted
+        self.race_start_time = rospy.Time.now()
+        
         #### ensure all dependent nodes are up and running ####
         rospy.wait_for_message(frame_topic, TFMessage) #wait until frame is published
         rospy.wait_for_message(sensors_topic, TORCSSensors) #also wait until sensors are published 
-#        rospy.wait_for_message(trajectory_topic, Path) #and wait for trajectory ()
         
         #### subscriptions ####
-        self.sub_trajectory = rospy.Subscriber(trajectory_topic, Path, self.trajectory_callback)
-        self.sub_sensors = rospy.Subscriber(sensors_topic, TORCSSensors, self.sensors_callback)
-        self.sub_frame = rospy.Subscriber(frame_topic, TFMessage, self.frame_callback, queue_size=1)
-#        self.sub_tf_listener = tf.TransformListener()
+        self.sub_trajectory = rospy.Subscriber(trajectory_topic, Path, self.trajectory_callback) #subscribe to selected trajectory in world frame
+        self.sub_sensors = rospy.Subscriber(sensors_topic, TORCSSensors, self.sensors_callback) #subscribe to sensor values for evaluation
+        self.sub_frame = rospy.Subscriber(frame_topic, TFMessage, self.frame_callback, queue_size=1) #subscribe to frame transformations
+        self.sub_speed = rospy.Subscriber(speed_topic, TwistStamped, self.speed_callback)
         
         
         #### publications ####
-        self.pub_ctrl = rospy.Publisher(ctrl_topic, TORCSCtrl, queue_size=10)
-        self.pub_needTrajectory = rospy.Publisher("/torcs_ros/ctrl_signal_action", Bool, queue_size=1)
+        self.pub_ctrl = rospy.Publisher(ctrl_topic, TORCSCtrl, queue_size=10) #publish control commands
+        self.pub_needTrajectory = rospy.Publisher("/torcs_ros/ctrl_signal_action", Bool, queue_size=1) #publish a control signal that indicates that a  new trajectory is needed
 
   
                 
@@ -79,13 +86,13 @@ class FollowTrajectory():
 
     #get trajectory
     def trajectory_callback(self, msg_trajectory):
-        self.trajectory = msg_trajectory
-        self.needForAction_msg.data = False
+        self.trajectory = msg_trajectory #get a new trajectory
+        self.needForAction_msg.data = False #set flag to false everytime a new trajectory has been received
         
     #get sensor values
     def sensors_callback(self, msg_sensors):
         self.sen_angle = msg_sensors.angle #angle in rad
-        self.sen_trackpos = msg_sensors.trackPos #normalized trackpos
+        self.sen_trackpos = msg_sensors.trackPos #normalized trackpos [-1, 1]
         self.sen_rpm = msg_sensors.rpm #rpm [no unit]
         self.sen_gear = msg_sensors.gear #current gear
         
@@ -94,8 +101,7 @@ class FollowTrajectory():
         frame_idx = 0 #index to identify the proper transformation if there is more than the world and base_link frames avalaible
         b_frameHasBeenFound = False #flag to show if transformation has been found
         
-#        [tf_trans, tf_rot] = self.sub_tf_listener.lookupTransform('world', 'base_link', rospy.Time(0)) #are identical
-#        print(self.ros_trans.x, tf_trans[0])
+
         #identify transformation
         for frame in msg_frame.transforms:
             if frame.header.frame_id == "world" and frame.child_frame_id == "base_link":
@@ -109,26 +115,27 @@ class FollowTrajectory():
             self.ros_rot.Set(msg_frame.transforms[frame_idx]) #Get rotation from published ros message
             # If new frame information has been published, new commands have to be calculated
             self.calculate_ctrl_commands() 
-            
+
+    def speed_callback(self, speed_msg):
+            self.sen_speedX = speed_msg.twist.linear.x
+           
     #calculate and publish control commands
     def calculate_ctrl_commands(self):
         if (len(self.trajectory.poses) > 0): #ensure this callback does not happen before the first trajectory has been published
             msg_ctrl = TORCSCtrl() #message container
             
-            [f_distToTraj, idx_poseHeading, f_distToEnd] = BaseLinkToTrajectory(
+            [f_distToTraj, idx_poseHeading, f_distToEnd, self.needForAction_msg.data] = BaseLinkToTrajectory(
                     self.ros_trans.x, self.ros_trans.y, self.trajectory.poses) #Calculate baselink to trajectory information
             self.trajectory_rot.Set(self.trajectory.poses[idx_poseHeading]) #Convert msg type to vec4() quatenrion
             f_deltaHeading = self.ComputeAngleDifference(self.ros_rot, self.trajectory_rot) #difference in desired orientation to current baselink orientation
             
-    #        print(f_distToEnd)
             print(f_deltaHeading, f_distToTraj, f_distToEnd)
     
-            ## f_distToBaselink und f_deltaHeading instead of trackpos and angle 
     
 #            self.ctrl_steering = (self.sen_angle - self.sen_trackpos*self.param_kappa)/self.param_steerLock #control towards midline
             
             #dist to trajectory has to have a sign
-            self.ctrl_steering = (f_deltaHeading + f_distToTraj/2*self.param_kappa)/self.param_steerLock
+            self.ctrl_steering = (f_deltaHeading + f_distToTraj*self.param_kappa)/self.param_steerLock
             
             #limit acceleration signal to achieve low constant speeds
             if (self.sen_rpm < 4000 or self.sen_gear == 0): #allow high enough rpm if car is in neutral gear (start of race)
@@ -146,47 +153,55 @@ class FollowTrajectory():
             self.pub_ctrl.publish(msg_ctrl) #publish
             
 
-            if (f_distToEnd < 1):
-                self.needForAction_msg.data = True
+            if (f_distToEnd < 1): #check whether end of trajectory has been reached (within a margin)
+                self.needForAction_msg.data = True #indicate a new trajectory is needed
 
-            self.pub_needTrajectory.publish(self.needForAction_msg)
-            self.CheckForOutOfTrack()
+            self.pub_needTrajectory.publish(self.needForAction_msg) #send message whether new control is needed or not
+            self.CheckForOutOfTrack() #restart server if vehicle is of track
 
-    #        bzConsoleIndicators.IamWorking() #indicate that node is running and messages are sent
-        else:
-            self.needForAction_msg.data = True
-            self.pub_needTrajectory.publish(self.needForAction_msg)
+        else: #no trajectory message has been received yet
+            self.needForAction_msg.data = True #indicate a new trajectory is needed
+            self.pub_needTrajectory.publish(self.needForAction_msg) #send message that new trajectory is needed
             
             
-    #Difference in angle [in rad] between vehicle orientation (baselink) to closest point on trajectory
+    #Difference in angle [in rad] between vehicle orientation (baselink) to closest point on trajectory 
+    #as we are working with a 2D assumption, the yaw value (angle around z) is the important value, as roll and pitch will be considered 0
     def ComputeAngleDifference(self, quad_baselink, quad_trajectory):
         [roll_bl, pitch_bl, yaw_bl] = tf.transformations.euler_from_quaternion(quad_baselink)
         [roll_tj, pitch_tj, yaw_tj] = tf.transformations.euler_from_quaternion(quad_trajectory)
         return yaw_tj-yaw_bl
         
+    #function that restarts a race if the vehicle is of track
     def CheckForOutOfTrack(self):
-        if (abs(self.sen_trackpos) > 1): #if vehicle is off track, restart 
-                msg_ctrl = TORCSCtrl() #message container
-                msg_ctrl.meta = 1 #set restart flag
-                #ensure last sent control command is no movement
-                msg_ctrl.accel = 0 
-                msg_ctrl.steering = 0
-                msg_ctrl.brake = 1
-                self.pub_ctrl.publish(msg_ctrl) #demand restart
-                
-                self.trajectory = Path() #reset selected trajectory to be empty
-                
-                os.system("rosnode kill /torcs_ros/torcs_ros_client_node") #kills client node with terminal command
-                print("Client node is being restarted as vehicle was off track")
-                rospy.sleep(1)
-                os.system("roslaunch torcs_ros_client torcs_ros_client_ns.xml") #relaunch client node
-                rospy.wait_for_message(self.sensors_topic, TORCSSensors) #wait for a message from client node to ensure it has restarted
-                #notify that new trajectory has to be selected as game has been resarted
-                self.needForAction_msg.data = True 
-                self.pub_needTrajectory.publish(self.needForAction_msg)
-                return;
-
+        b_Restart = False
+        if (abs(self.sen_trackpos) > 1): #if vehicle is off track or last point is , restart 
+                b_Restart = True
+        if (self.sen_speedX < 3): #if low speed, car is probably stuck
+            dt = rospy.Time.now().secs - self.race_start_time.secs #calculate whether race has just started
+            if (dt > 15): #do not restart if race has just started
+                b_Restart = True 
+        if b_Restart == True:
+            msg_ctrl = TORCSCtrl() #message container
+            msg_ctrl.meta = 1 #set restart flag
+            #ensure last sent control command is no movement
+            msg_ctrl.accel = 0 
+            msg_ctrl.steering = 0
+            msg_ctrl.brake = 1
+            self.pub_ctrl.publish(msg_ctrl) #demand restart
             
+            self.trajectory = Path() #reset selected trajectory to be empty
+            
+            os.system("rosnode kill /torcs_ros/torcs_ros_client_node") #kills client node with terminal command
+            print("Client node is being restarted as vehicle was off track")
+            rospy.sleep(1) #give the system enough time to have killed of node
+#            os.system("roslaunch torcs_ros_client torcs_ros_client_ns.xml") #relaunch client node with roslaunch command when not in namespace yet (if called manually from console)
+            os.system("roslaunch torcs_ros_client torcs_ros_client.xml") #relaunch client node with roslaunch command when in namespace (if launched with bringup .launch file)
+            rospy.wait_for_message(self.sensors_topic, TORCSSensors) #wait for a message from client node to ensure it has restarted
+            #notify generation node that new trajectory has to be selected as game has been resarted
+            self.needForAction_msg.data = True  
+            self.pub_needTrajectory.publish(self.needForAction_msg)
+            self.race_start_time = rospy.Time.now() #new race started, reinitialize time
+
 if __name__ == "__main__":
     rospy.init_node("Trajectory_Follow_Control")
     Controller = FollowTrajectory()
