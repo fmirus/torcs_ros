af0763bd96f381dd0d9f36d05fe615369746b6c6
B-Zorn <benni@the-zorns.de>
2018-07-30 10:22:44 +0200

    Progress in Q-Associative network. State input does not subscribe itself anymore

--- torcs_ros_trajectory_selection/src/torcs_ros_trajectory_selection.py
+++ torcs_ros_trajectory_selection/src/torcs_ros_trajectory_selection.py
@@ -10,6 +10,7 @@ import numpy as np
 import rospy
 import nengo
 import subprocess
+import copy
 
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import LaserScan
@@ -26,28 +27,32 @@ cwd = rospkg.RosPack().get_path('torcs_ros_trajectory_gen')
 sys.path.append(cwd[:-24] + "common")
 cwd = cwd[:-24]
 
-from bzReadTrajectoryParams import readTrajectoryParams
+from bzReadTrajectoryParams import readTrajectoryParams, calcTrajectoryAmount
 
 #A class that can be passed to a nengo node as input
 #It subscribes to the scan topic in order to create the nengo input
 class NodeInputScan():
-    def __init__(self, scan_topic = "/torcs_ros/scan_track"):
+    def __init__(self, length, scan_topic = "/torcs_ros/scan_track"):
          #### various parameters and variables ####
         #choose index of scanners to use
-        #angle min/max: +-1.57; increment 0.1653; instantenous, range: 200 m
+        #angle min/max: +-1.57; increment 0.1653; instantenous; range: 200 m
+
         self.a_selectScanTrack = [3, 6, 11, -7, -6]
         self.param_rangeNormalize = 100 #value used for normalization. all values above will be considered as 1   
   
         #### subscription parameters ####      
         self.a_scanTrack = []
-        [self.a_scanTrack.append(0) for idx in self.a_selectScanTrack]
+#        [self.a_scanTrack.append(-1) for idx in self.a_selectScanTrack]
+        [self.a_scanTrack.append(-1) for idx in range(length)]
+
         
         ##### subscribers #####
         self.sub_scanTrack = rospy.Subscriber(scan_topic, LaserScan, self.scan_callback)
         
     #Get values from message, normalize them and clip them between 0 and 1
     def scan_callback(self, msg_scan):
-        self.a_scanTrack = [np.clip(msg_scan.ranges[idx]/self.param_rangeNormalize, 0, 1) for idx in self.a_selectScanTrack]
+        pass
+#        self.a_scanTrack = [np.clip(msg_scan.ranges[idx]/self.param_rangeNormalize, 0, 1) for idx in self.a_selectScanTrack]
         
     #Call function needed for nengo, called within each simulation step 
     #returns a constant input over the entirety of the simulation dependent on the sensor state when starting the simulation
@@ -61,6 +66,26 @@ class NodeInputScan():
     #Needed for nengo net to be parametrizable
     def __len__(self):
         return len(self.a_selectScanTrack)
+    
+    def setVals(self, ia_scans):
+        self.a_scanTrack = ia_scans
+        
+        
+#A class that can be passed to a nengo node as input
+#Gets the reward vector from ros whenever a trajectory has been completed
+#Returns that reward vector on every timestep with the __call__ function        
+class NodeInputReward():
+    def __init__(self, n_action):
+        self.reward = n_action*[0] #initialize reward to be 0 for all actions
+        
+    def __call__(self, t):
+        return self.reward #return current reward
+    
+    def NoLearning(self):
+        self.reward = [0 for action in self.reward] #reset reward array to 0
+        
+    def RewardAction(self, i, i_reward):
+        self.reward[i] = i_reward #set a specific action to have a reward
 
 #A class that can be passed to a nengo node as an output probe
 #Member variables only save last state, therefore eliminating a need for a nengo.Probe or similar
@@ -84,32 +109,37 @@ class TrajectorySelector():
         #angle min/max: +-1.57; increment 0.1653; instantenous, range: 200 m
         self.a_selectScanTrack = [3, 6, 11, -7, -6]
         self.param_rangeNormalize = 100 #value used for normalization. all values above will be considered as 1
-        self.param_f_maxExpectedSpeed = 35.0 #[km/h]
-        [self.param_f_longitudinalDist, _, _] = readTrajectoryParams(cwd)
+        self.param_f_maxExpectedSpeed = 37.0 #[km/h], set higher than 34 deliberately to scale reward a bit
+        [self.param_f_longitudinalDist, _, self.param_n_action] = readTrajectoryParams(cwd)
+        self.param_n_action = calcTrajectoryAmount(self.param_n_action)
         self.calculateRewardRange()
         
         #### subscription parameters ####
         self.b_TrajectoryNeeded = False
         self.f_lapTimeStart = 0
-        self.f_lapTimeEnd = 0 #not really needed
         self.f_lapTimeCurrent = 0 
+        self.f_lapTimePrevious = 0#needed for lap change
         self.f_distStart = 0
-        self.f_distEnd = 0 #not really needed
         self.f_distCurrent = 0
+        self.f_distPrevious = 0
         self.f_speedXCurrent = 0
         self.f_speedXStart = 0
         self.a_scanTrack = []
-        [self.a_scanTrack.append(0) for idx in self.a_selectScanTrack]
+        [self.a_scanTrack.append(-1) for idx in self.a_selectScanTrack]
         self.b_handshake = False
-        
+
         
         #### nengo net and parameters #### 
-        self.output_prober = NodeOutputProber(21) #naction currently hardocded, should be a global ros parameter
-        self.q_net_ass = snn.qnet_associative(True, NodeInputScan(), self.output_prober.ProbeFunc)
+        self.state_inputer = NodeInputScan(len(self.a_selectScanTrack))
+        self.reward_inputer = NodeInputReward(self.param_n_action)
+        self.output_prober = NodeOutputProber(self.param_n_action) #naction currently hardocded, should be a global ros parameter
+        self.q_net_ass = snn.qnet_associative(False, self.state_inputer, self.reward_inputer, 
+                                              self.output_prober.ProbeFunc, self.param_n_action)
         nengo.rc.set('progress', 'progress_bar', 'nengo.utils.progress.TerminalProgressBar') #Terminal progress bar for inline
-        self.sim = nengo.Simulator(self.q_net_ass, progress_bar=True)
+        self.sim = nengo.Simulator(self.q_net_ass, progress_bar=False)
         self.b_doSimulateOnce = True
         self.idx_action = 0
+        
         ##### subscribers #####
         self.sub_scanTrack = rospy.Subscriber(scan_topic, LaserScan, self.scan_callback)
         self.sub_needForAction = rospy.Subscriber(action_topic, Bool, self.needForAction_callback)
@@ -117,13 +147,13 @@ class TrajectorySelector():
         self.sub_speed = rospy.Subscriber(speed_topic, TwistStamped, self.speed_callback)
         self.sub_handshake = rospy.Subscriber("/torcs_ros/gen2selHandshake", Bool, self.handshake_callback)
         self.sub_ctrlCmd = rospy.Subscriber(ctrl_topic, TORCSCtrl, self.ctrl_callback)
+        
         #### publisher ####
         self.pub_trajectorySelection = rospy.Publisher("/torcs_ros/TrajectorySelector", Int8, queue_size=1) #negative values are parsed when no trajectory should be selected
         
         
     def scan_callback(self, msg_scan):
         self.a_scanTrack = [np.clip(msg_scan.ranges[idx]/self.param_rangeNormalize, 0, 1) for idx in self.a_selectScanTrack]
-#        print(self.a_scanTrack)
 
     def needForAction_callback(self, msg_action):
         self.b_TrajectoryNeeded = msg_action.data
@@ -144,7 +174,7 @@ class TrajectorySelector():
                 #needs xdotool
                 #os.system('xdotool search --name "torcs-bin" key p')
                 #subprocess.call('xdotool search --name "torcs-bin" key p', shell=True) 
-                
+                self.state_inputer.setVals(self.a_scanTrack) #state values only changed before action selection simulation
                 self.sim.run(0.5) #run simulation for x 
                 self.idx_action = np.argmax(np.array(self.output_prober.probe_vals[:-1]))
                 self.b_doSimulateOnce = False #don't simualte again until this trajectory has been published (which means that the action signal will turn to False)
@@ -158,8 +188,18 @@ class TrajectorySelector():
             ##reset do once flag, a new trajectory has been published
 
     def sensors_callback(self, msg_sensors):
+        self.f_lapTimePrevious = self.f_lapTimeCurrent
         self.f_lapTimeCurrent = msg_sensors.currentLapTime
+        
+        self.f_distPrevious = self.f_distCurrent
         self.f_distCurrent = msg_sensors.distFromStart
+        
+        if(self.f_lapTimeCurrent < self.f_lapTimeStart): 
+            self.f_lapTimeStart = -(self.f_lapTimePrevious - self.f_lapTimeStart)
+        if(self.f_distCurrent*10 < self.f_distStart): #*10 ensures condition to only hold at lap change
+            self.f_distStart = -(self.f_distPrevious - self.f_distStart)
+            print("Crossing start line")
+            
         
     def speed_callback(self, msg_speed):
         self.f_speedXCurrent = msg_speed.twist.linear.x
@@ -169,23 +209,38 @@ class TrajectorySelector():
         
     def ctrl_callback(self, msg_ctrl):
         self.b_handshake = True
+        if(msg_ctrl.meta == 1):
+            print("A restart has been requested")
+            self.clearMemory()
         
         
     #calculate the lowest amount of time needed at the expected speed to traverse the longitudinal distance if the road were to be straight
     #this will then be used to calculate the reward
     #higher values can be achieved in curves, but it is not absoulutely necessary to limit this value to one
     def calculateRewardRange(self):
-        self.param_f_minTime = self.param_f_longitudinalDist / self.param_f_maxExpectedSpeed*1000.0/3600
+        self.param_f_minTime = self.param_f_longitudinalDist / (self.param_f_maxExpectedSpeed*1000.0/3600)
+        
         
         
     def calculateReward(self):
         if (self.f_speedXCurrent < 30 or self.f_speedXStart < 30): #disregard when end speed has not been reached yet
-            self.reward = [0]
-        if (self.f_lapTimeCurrent > self.f_lapTimeStart): #ensure no new lap has started
+            self.reward = 0
+        elif (self.f_lapTimeCurrent > self.f_lapTimeStart): #ensure no new lap has started
             f_distTravelled = self.f_distCurrent - self.f_distStart #
             f_timeNeeded = self.f_lapTimeCurrent - self.f_lapTimeStart #can be neglected if we 
-            self.reward = (f_distTravelled/self.param_f_longitudinalDist) / (f_timeNeeded/self.param_f_minTime)
+            self.reward = (f_distTravelled/self.param_f_longitudinalDist) / (f_timeNeeded/self.param_f_minTime) #maybe pow 2
+            self.reward *= self.reward #scale reward for more distinction between all trajectories
+        self.checkRewardValidity()
+        
+        print(self.reward)
 
+        
+    def checkRewardValidity(self):
+        if (self.reward > 5):
+            self.reward = 0
+            
+    def clearMemory(self):
+        pass
 
 if __name__ == "__main__":
     if(len(os.popen("dpkg -l | grep xdotool").readlines()) == 0): #check whether xdotool is installed
